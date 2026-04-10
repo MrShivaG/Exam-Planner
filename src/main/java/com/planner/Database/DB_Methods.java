600dbb218fb29bb36ce67e296045d08b78714a3a
krashnkantchaurasia123-design <krashnkantchaurasia123@gmail.com>
2026-04-10 23:47:26 +0530

    updated DB_methods class

--- src/main/java/com/planner/Database/DB_Methods.java
+++ src/main/java/com/planner/Database/DB_Methods.java
@@ -3,6 +3,7 @@ package com.planner.Database;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.*;
 
 public class DB_Methods {
     static database databaseobj = new database();
@@ -15,20 +16,39 @@ public class DB_Methods {
         System.out.println(databaseobj.connectDB());
     }
 
-
-    public void insertData(int room_no, int rows, int column, int capacity, boolean availability) throws SQLException {
-
+    public void insertData(int room_no, int rows, int column, boolean availability) throws SQLException {
         PreparedStatement ps = con.prepareStatement("insert into class_room(room_no, class_row, class_column, capacity, availability) values(?, ?, ?, ?, ?)");
-
-        ps.setInt(1,room_no);
-        ps.setInt(2,rows);
-        ps.setInt(3,column);
-        ps.setInt(4,capacity);
-        ps.setBoolean(5,availability);
-
+        ps.setInt(1, room_no);
+        ps.setInt(2, rows);
+        ps.setInt(3, column);
+        ps.setInt(4, rows*column);
+        ps.setBoolean(5, availability);
         ps.executeUpdate();
         con.close();
+    }
+
+    public void deleterow(int room_no) throws SQLException {
+        PreparedStatement ps = con.prepareStatement("delete from class_room where room_no = ?");
+        ps.setInt(1, room_no);
+        ps.executeUpdate();
+        con.close();
+    }
+
+    public void updatedata(int room_no) throws SQLException {
+        PreparedStatement ps = con.prepareStatement("delete from class_room where room_no = ?");
 
 
+    }
+
+    public int[] fetchRowColumn(int room_no) throws SQLException {
+        PreparedStatement ps = con.prepareStatement("select * from class_room where room_no = ?");
+        ps.setInt(1, room_no);
+        ResultSet rs = ps.executeQuery();
+        if (rs.next()) {
+            int row = rs.getInt("class_row");
+            int column = rs.getInt("class_column");
+            return new int[]{row, column};
+        }
+        return new int[]{-1, -1};
     }
 }
