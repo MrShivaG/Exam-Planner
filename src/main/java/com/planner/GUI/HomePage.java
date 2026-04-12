3ec055fe0f6a440f4eb705fd28c0e3f350baabc2
krashnkantchaurasia123-design <krashnkantchaurasia123@gmail.com>
2026-04-12 14:54:09 +0530

    create homepage class

---
+++ src/main/java/com/planner/GUI/HomePage.java
@@ -0,0 +1,12 @@
+package com.planner.GUI;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import org.apache.poi.sl.usermodel.ObjectMetaData;
+
+public class HomePage extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
