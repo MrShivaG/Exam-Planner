2421981eaf62c56a21349449a45e8d4b4d7eca91
MrShivaG <shivaahirwar2005@gmail.com>
2026-04-12 18:58:41 +0530

    ArrangementsDB will be used for storing arrangements

---
+++ src/main/java/com/planner/Database/ArrangementsDB.java
@@ -0,0 +1,27 @@
+package com.planner.Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ArrangementsDB {
+
+    private static final String URL = "jdbc:mysql://192.168.1.169:3306/arrangements";
+    private static final String USERNAME = "remote";
+    private static final String PASSWORD = "root";
+
+
+    public static String connectDB(){
+        try {
+            Connection con = connection();
+            return "Database connected successfully.";
+        } catch (SQLException e) {
+            return "Connection failed: " + e.getMessage();
+        }
+    }
+    public static Connection connection() throws SQLException {
+        Connection con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        return con;
+    }
+
+}
