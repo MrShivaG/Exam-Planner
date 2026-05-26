dd41ae843eff16391520d17a1901609800af0a57
Jaychaurasia <jaychaurasia30@gmail.com>
2026-05-26 11:35:27 +0530

    updated 1 functions

--- src/main/java/com/planner/Database/ArrangementsDB.java
+++ src/main/java/com/planner/Database/ArrangementsDB.java
@@ -8,11 +8,11 @@ import static com.planner.Database.DB_Methods.con;
 
 public class ArrangementsDB {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/arrangements";
+    private static final String URL = "jdbc:mysql://10.51.136.20:3306/arrangements";
     //10.225.181.20 shiva
     //10.51.136.20 jay
 
-    private static final String USERNAME = "root";
+    private static final String USERNAME = "remote";
     private static final String PASSWORD = "root";
 
 
@@ -53,6 +53,18 @@ public class ArrangementsDB {
             }
             e.printStackTrace();
             return null;
+        }
+    }
+
+    public static void softDeleteStudent(String tableName, String enrollNo) {
+        try (Connection con = connection()) {
+            PreparedStatement ps = con.prepareStatement(
+                    "UPDATE `" + tableName + "` SET Enroll_no = 'Null', SubCode = 'Null', Status = 'Null' WHERE Enroll_no = ?"
+            );
+            ps.setString(1, enrollNo);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
